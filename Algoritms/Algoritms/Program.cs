cda0ee55471e58b472fbf262e1bdac1bdc3a00bc
KimHJo <dreamybaby@naver.com>
2019-08-02 14:56:39 +0900

    딕셔너리 제거, 바로바로 처리하기 위해 입력한 A B 라인에 따라 계산하여 출력 - 에러목록 - 1. 항상 1번컴퓨터만 출력 2. 값이 정상적으로 들어가는 지 모름

--- Algoritms/Algoritms/Program.cs
+++ Algoritms/Algoritms/Program.cs
@@ -1,21 +1,53 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Algoritms {
+
+    public class AB {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public AB(int a, int b) {
+            this.A = a;
+            this.B = b;
+        }
+    }
     class Program {
         static void Main(string[] args) {
-
-            var testCase = 0;
-            testCase = int.Parse(Console.ReadLine());
+            var testCase = int.Parse(Console.ReadLine());
 
             var abLines = new List<string>();
-            for (int i = 0; i < testCase; i++)
+
+            for (int i = 0; i < testCase; i++) {
                 abLines.Add(Console.ReadLine());
 
+                var temp = abLines[i].Split(' ');
 
-            Console.WriteLine(testCase);
-            foreach (var line in abLines)
-                Console.WriteLine(line);
+                var A = int.Parse(temp[0]) % 10 + 1;
+                var B = int.Parse(temp[1]) % 10 + 1;
+
+                switch (A) {
+                    case 1:
+                    case 5:
+                    case 6:
+                        Console.WriteLine("1번 컴퓨터");
+                        break;
+                    case 3:
+                        Console.WriteLine((B / 5 + 1) + "번 컴퓨터");
+                        break;
+                    case 2:
+                    case 4:
+                    case 7:
+                    case 8:
+                        Console.WriteLine((B / 4 + 1) + "번 컴퓨터");
+                        break;
+                    case 9:
+                        Console.WriteLine((B / 2 + 1) + "번 컴퓨터");
+                        break;
+                }
+            }
         }
     }
 }
