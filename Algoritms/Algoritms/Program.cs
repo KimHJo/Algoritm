8a08437cea549a45a6ceba62be92ba0b985dcb6c
KimHJo <dreamybaby@naver.com>
2019-08-02 14:06:49 +0900

    테스트케이스 수 및 데이터 수를 구하기위한 변수 선언 및 값 입력

---
+++ Algoritms/Algoritms/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algoritms {
+    class Program {
+        static void Main(string[] args) {
+
+            var testCase = 0;
+            testCase = int.Parse(Console.ReadLine());
+
+            var abLines = new List<string>();
+            for (int i = 0; i < testCase; i++)
+                abLines.Add(Console.ReadLine());
+
+
+            Console.WriteLine(testCase);
+            foreach (var line in abLines)
+                Console.WriteLine(line);
+        }
+    }
+}
