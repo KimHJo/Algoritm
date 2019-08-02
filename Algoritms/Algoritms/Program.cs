fe53d741b95f24a9ff1da0edbb3991becff3f401
KimHJo <dreamybaby@naver.com>
2019-08-02 15:30:47 +0900

    문제 해결 간략화 시작

--- Algoritms/Algoritms/Program.cs
+++ Algoritms/Algoritms/Program.cs
@@ -4,49 +4,26 @@ using System.Linq;
 using System.Collections.Generic;
 
 namespace Algoritms {
-
-    public class AB {
-        public int A { get; set; }
-        public int B { get; set; }
-
-        public AB(int a, int b) {
-            this.A = a;
-            this.B = b;
-        }
-    }
     class Program {
         static void Main(string[] args) {
             var testCase = int.Parse(Console.ReadLine());
 
             var abLines = new List<string>();
 
+            var answerList = new List<int>();
             for (int i = 0; i < testCase; i++) {
                 abLines.Add(Console.ReadLine());
 
                 var temp = abLines[i].Split(' ');
 
-                var A = int.Parse(temp[0]) % 10 + 1;
-                var B = int.Parse(temp[1]) % 10 + 1;
+                var A = int.Parse(temp[0]) % 10;
+                var B = int.Parse(temp[1]) % 10;
 
-                switch (A) {
-                    case 1:
-                    case 5:
-                    case 6:
-                        Console.WriteLine("1번 컴퓨터");
-                        break;
-                    case 3:
-                        Console.WriteLine((B / 5 + 1) + "번 컴퓨터");
-                        break;
-                    case 2:
-                    case 4:
-                    case 7:
-                    case 8:
-                        Console.WriteLine((B / 4 + 1) + "번 컴퓨터");
-                        break;
-                    case 9:
-                        Console.WriteLine((B / 2 + 1) + "번 컴퓨터");
-                        break;
-                }
+                answerList.Add((int)Math.Pow(A, B) % 10);
+            }
+
+            foreach(var c in answerList) {
+                Console.WriteLine("{0}번 컴퓨터가 처리합니다.", c);
             }
         }
     }
