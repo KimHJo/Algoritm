12f3ea3c1058398b3e9d28ca4dbb6ceb93e3370b
KimHJo <dreamybaby@naver.com>
2019-08-02 17:31:36 +0900

    백준 1009번 알고리즘 완료

--- Algoritms/Algoritms/Program.cs
+++ Algoritms/Algoritms/Program.cs
@@ -7,23 +7,35 @@ namespace Algoritms {
             int testCaseNumber = int.Parse(Console.ReadLine());
 
             string[] abLines = new string[testCaseNumber];
-            
+
+            int[] answer = new int[testCaseNumber];
+
             for (int i = 0; i < testCaseNumber; i++) {
                 abLines[i] = Console.ReadLine();
                 string[] temp = abLines[i].Split(' ');
 
                 int A = int.Parse(temp[0]) % 10;
-                int B = int.Parse(temp[1]) % 10;
+                int B = int.Parse(temp[1]) % 4;
+                if (A == 0) {
+                    answer[i] = 10;
+                } else {
+                    switch (B) {
+                        case 0:
+                            answer[i] = A * A * A * A % 10; ;
+                            break;
+                        case 1:
+                            answer[i] = A % 10;
+                            break;
+                        case 2:
+                            answer[i] = A * A % 10;
+                            break;
+                        case 3:
+                            answer[i] = A * A * A % 10;
+                            break;
+                    }
+                }
 
-                //백준에서 이렇게 쓰면 정답이 아니란다... 그리고 코드길이가 길어졌네 당연한건가.
-                //int t = (int)Math.Pow(A, B) % 10;
-                //abLines[i] = t.ToString();
-
-                // 계산이 너무 오래걸린다.
-                //answerList.Add((int)Math.Pow(A, B) % 10);
-            }
-            foreach(string t in abLines) {
-                Console.WriteLine(t);
+                Console.WriteLine(answer[i]);
             }
         }
     }
