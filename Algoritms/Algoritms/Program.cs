690014ff88f05599f4d279ed58953ea07982a791
KimHJo <dreamybaby@naver.com>
2019-08-05 11:47:13 +0900

    1차 실패

--- Algoritms/Algoritms/Program.cs
+++ Algoritms/Algoritms/Program.cs
@@ -15,27 +15,42 @@ namespace Algoritms {
                 {1, 7, 3 },
             };
 
-            var temp = new List<int>();
-            var answer = new List<int>();
 
-            for (int i = 0; i < Command.Length / 3; i++) {
-                for (int j = Command[i, 0]; j <= Command[i, 1]; j++) {
-                    temp.Add(Array[j - 1]);
-                }
-                temp.Sort();
-                answer.Add(temp[Command[i, 2]-1]);
-                temp.Clear();
-            }
+            //foreach(var item in solution(Array, Command)) {
+            //    Console.WriteLine(item);
+            //}
+            //var temp = new List<int>();
+            //var answer = new List<int>();
 
-            foreach (var i in answer) {
-                Console.WriteLine(i);
-            }
+            //for (int i = 0; i < Command.Length / 3; i++) {
+            //    for (int j = Command[i, 0]; j <= Command[i, 1]; j++) {
+            //        temp.Add(Array[j - 1]);
+            //    }
+            //    temp.Sort();
+            //    answer.Add(temp[Command[i, 2]-1]);
+            //    temp.Clear();
+            //}
+
+            //foreach (var i in answer) {
+            //    Console.WriteLine(i);
+            //}
 
         }
         public static int[] solution(int[] array, int[,] commands) {
-            int[] answer = new int[] { };
+            List<int> answer = new List<int>();
 
-            return answer ;
+            var temp = new List<int>();
+
+            for(int i = 0; i < commands.Length/3; i++) {
+                for(int j = commands[i,0]; j < commands[i,1]; j++) {
+                    temp.Add(array[j - 1]);
+                }
+                temp.Sort();
+                answer.Add(temp[commands[i, 2] - 1]);
+                temp.Clear();
+            }
+
+            return answer.ToArray();
 
         }
     }
