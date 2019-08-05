711475dca81fb1179647c73d95c53a9a43807543
KimHJo <dreamybaby@naver.com>
2019-08-05 14:49:07 +0900

    배열을 돌려보려고 했음

--- Algoritms/Algoritms/Program.cs
+++ Algoritms/Algoritms/Program.cs
@@ -11,12 +11,33 @@ namespace Algoritms {
             int[] Numbers = new int[] { 6, 10, 2, };
             int[] Numbers2 = new int[] { 3, 30, 34, 5, 9, };
 
-            foreach(var i in Numbers) {
-                Console.WriteLine(i);
-            }
+            solution(Numbers);
 
         }
         public static string solution(int[] numbers) {
+            var numberList = numbers.ToList();
+            numberList.AddRange(numberList);
+
+
+            int i = 0;
+            while (true){
+                if (i == numbers.Length) break;
+                int j = i;
+
+                for (j = i; j < numbers.Length + i; j++) {
+                    Console.Write(j);
+                }
+                Console.WriteLine("증가");
+
+                for (j = numbers.Length + i - 1; j >= i; j--) {
+                    Console.Write(j);
+                }
+                Console.WriteLine("감소");
+
+                i++;
+            }
+
+
             string answer = "";
             return answer;
         }
