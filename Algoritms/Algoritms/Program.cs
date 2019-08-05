78334a8d1f1ab2934a220619d5f1429809fb871f
KimHJo <dreamybaby@naver.com>
2019-08-05 13:38:51 +0900

    프로그래머스 정렬 K번째 수 문제 해결 - 코드 리펙토링 시작

--- Algoritms/Algoritms/Program.cs
+++ Algoritms/Algoritms/Program.cs
@@ -19,41 +19,20 @@ namespace Algoritms {
             foreach (var item in solution(Array, Command)) {
                 Console.WriteLine(item);
             }
-
-            //var temp = new List<int>();
-            //var answer = new List<int>();
-
-            //for (int i = 0; i < Command.Length / 3; i++) {
-            //    for (int j = Command[i, 0]; j <= Command[i, 1]; j++) {
-            //        temp.Add(Array[j - 1]);
-            //    }
-            //    temp.Sort();
-            //    answer.Add(temp[Command[i, 2]-1]);
-            //    temp.Clear();
-            //}
-
-            //foreach (var i in answer) {
-            //    Console.WriteLine(i);
-            //}
-
         }
         public static int[] solution(int[] array, int[,] commands) {
             List<int> answer = new List<int>();
 
             var temp = new List<int>();
 
-            foreach(var i in array) {
-                answer.Add(i);
+            for(int i = 0; i < commands.Length/3; i++) {
+                for(int j = commands[i, 0] - 1; j <= commands[i, 1]-1; j++) {
+                    temp.Add(array[j]);
+                }
+                temp.Sort();
+                answer.Add(temp[commands[i, 2] - 1]);
+                temp.Clear();
             }
-
-            //for(int i = 0; i < commands.Length/3; i++) {
-            //    for(int j = commands[i,0]; j < commands[i,1]; j++) {
-            //        temp.Add(array[j - 1]);
-            //    }
-            //    temp.Sort();
-            //    answer.Add(temp[commands[i, 2] - 1]);
-            //    temp.Clear();
-            //}
 
             return answer.ToArray();
 
