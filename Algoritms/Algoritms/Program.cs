bb906b3085d26165b175b8f704298a26ab9e1154
KimHJo <dreamybaby@naver.com>
2019-08-05 16:25:48 +0900

    Array.Sort를 이용하여 간단히 정렬 가능

--- Algoritms/Algoritms/Program.cs
+++ Algoritms/Algoritms/Program.cs
@@ -11,26 +11,47 @@ namespace Algoritms {
             int[] Numbers = new int[] { 6, 10, 2, };
             int[] Numbers2 = new int[] { 3, 30, 34, 5, 9, };
 
-            Console.WriteLine(solution(Numbers2));
+            int[] Numbers3 = new int[] { 0,0,0 };
+
+            Console.WriteLine(solution(Numbers3));
+
+            Array.Sort(Numbers, (x, y) => {
+                string xy = x.ToString() + y.ToString();
+                string yx = y.ToString() + x.ToString();
+                return yx.CompareTo(xy);
+            });
+
+            if (Numbers.Where(x => x == 0).Count() == Numbers.Length)
+                Console.WriteLine("DDD");
+            else
+                Console.WriteLine(string.Join("", Numbers));
             
         }
         public static string solution(int[] numbers) {
-
             int numleng = numbers.Length;
-            for (int k = 0; k < numbers.Length; k++) {
-                for (int i = 0; i < numbers.Length - 1; i++) {
-                    var str1 = numbers[i].ToString() + numbers[i + 1];
-                    var str2 = numbers[i + 1].ToString() + numbers[i].ToString();
+            List<string> numList = new List<string>();
 
-                    if (int.Parse(str1) >= int.Parse(str2))
-                        swap(numbers, i, i + 1);
+            foreach(int c in numbers) {
+                numList.Add(c.ToString());
+            }
+
+            for(int i = 0; i < numleng; i++) {
+                for(int j = 0; j < numleng -1; j++) {
+                    string str1 = numList[j] + numList[j + 1];
+                    string str2 = numList[j + 1] + numList[j];
+
+                    if(Int64.Parse(str1) >= Int64.Parse(str2)) {
+                        swap(numbers, j, j + 1);
+                    }
                 }
             }
             string answer = "";
 
-            foreach(var c in numbers.Reverse()) {
+            foreach(int c in numbers.Reverse()) {
                 answer += c.ToString();
             }
+            if (int.Parse(answer) == 0)
+                answer = "0";
             return answer;
         }
         public static void swap(int[] array, int a, int b) {
