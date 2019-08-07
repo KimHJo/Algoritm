61f76b61f9d30b5e1fc6fcbba4cf84ed74d5a499
KimHJo <dreamybaby@naver.com>
2019-08-07 14:19:22 +0900

    프로그래머스 C# 연습문제 Level1 김서방찾기, 문자열내림차순

--- Algoritms/Algoritms/Program.cs
+++ Algoritms/Algoritms/Program.cs
@@ -7,6 +7,20 @@ using System.Collections.Generic;
 namespace Algoritms {
     class Program {
         static void Main(string[] args) {
+            string s = "AB";
+
+            Console.WriteLine(solution(s, 1));
+        }
+        //65~ 90 A-Z
+        //97~ 122 a-z
+        // 26
+        // n  = 1~25
+
+        public static string solution(string s, int n) {
+            string[] sArray = s.Split(' ').ToArray();
+
+
+
         }
     }
 }
