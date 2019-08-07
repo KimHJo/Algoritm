2211092113ab601064e8806a9fdf23a65870f85b
KimHJo <dreamybaby@naver.com>
2019-08-07 17:51:55 +0900

    프로그래머스 C# 연습문제 Level1 예산, 콜라츠추측, 평균구하기

--- Algoritms/Algoritms/Program.cs
+++ Algoritms/Algoritms/Program.cs
@@ -7,9 +7,27 @@ using System.Collections.Generic;
 namespace Algoritms {
     class Program {
         static void Main(string[] args) {
-            //Console.WriteLine(solution(100));
+            Console.WriteLine(solution(10));
+        }
+        public static bool solution(int x) {
+            bool answer = true;
+
+            var ar = x.ToString().ToArray();
+
+            int sum = 0;
+            foreach(var i in ar) {
+                sum += i;
+            }
+
+            
+            if (x % sum == 0) {
+                answer = true;
+            } else {
+                answer = false;
+            }
+
+            return answer;
         }
 
     }
-
 }
