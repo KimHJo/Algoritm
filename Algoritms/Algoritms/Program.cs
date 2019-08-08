9e43aae5b659985b89dccee245ec43853587d9a0
KimHJo <dreamybaby@naver.com>
2019-08-08 14:46:43 +0900

    조건문 처리

--- Algoritms/Algoritms/Program.cs
+++ Algoritms/Algoritms/Program.cs
@@ -12,8 +12,33 @@ namespace Algoritms {
         }
         public static string solution(string number, int k) {
             //char[] temp = number.ToArray();
+            char[] numArray = number.ToArray();
 
             //int numLen = number.ToString().Length;
+            int numLen = number.Length;
+
+            for (int i = 0; i < k; i++) {
+                for (int j = 0; j < numLen - 1; j++) {
+                    if (numArray[j] == ' ')
+                        break;
+
+                    for (int s = j + 1; s < numLen; s++) {
+                        if (numArray[s] == ' ')
+                            s++;
+
+                        if (numArray[j] == numArray[s]) {
+                            s++;
+                        } else if (numArray[j] > numArray[s]) {
+                            numArray[s] = ' ';
+                            break;
+                        } else if (numArray[j] < numArray[s]) {
+                            numArray[j] = ' ';
+                            break;
+                        }
+                    }
+                }
+            }
+
 
             //for (int i = 0; i < k; i++) {
             //    for (int j = 0; j < numLen - 1; j++) {
@@ -35,13 +60,14 @@ namespace Algoritms {
             //}
 
             //string answer = "";
-
-            //foreach (char c in temp) {
-            //    if (c != ' ') {
-            //        answer += c;
-            //    }
-            //}
             string answer = "";
+
+            foreach (char c in numArray) {
+                if (c != ' ') {
+                    answer += c;
+                }
+            }
+
             return answer;
         }
 
