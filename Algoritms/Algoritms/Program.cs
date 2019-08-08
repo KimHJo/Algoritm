6834f3fd7118881ba0a8bed8c9367ef1165531e9
KimHJo <dreamybaby@naver.com>
2019-08-08 17:47:20 +0900

    반복문 수정, 같은값 다음 조건처리 시작

--- Algoritms/Algoritms/Program.cs
+++ Algoritms/Algoritms/Program.cs
@@ -3,30 +3,62 @@ using System.Collections;
 using System.Linq;
 using System.IO;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Algoritms {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine(solution("1924", 2));
+            //Console.WriteLine(solution("1924", 2));
             Console.WriteLine(solution("4177252841", 4));
         }
         public static string solution(string number, int k) {
-            string answer = "";
-            int count = k;
-            var numList = number.ToList();
             if (number.All(x => x == '0'))
-                return answer = "0";
+                return "0";
 
-            while(count > 0) {
-                var list = numList.GetRange(0, k);
+            char[] numArray = number.ToArray();
 
-                var min = list.Min().ToString();
-                numList.Remove(Convert.ToChar(min));
-                count--;
+            int count = 0;
+            while (count < k) {
+
+                for (int i = 0; i < numArray.Length-1; i++) {
+                    if (numArray[i] == '0')
+                        continue;
+
+                    for (int j = i+1; j < numArray.Length; j++) {
+                        if (numArray[j] == '0')
+                            continue;
+
+                        //Console.WriteLine("{0} {1} {2} {3}", i, j, numArray[i], numArray[j]);
+
+                        if(numArray[i] == numArray[j]) {
+
+                        }
+
+                        if(numArray[i] < numArray[j]) {
+                            numArray[i] = '0';
+                            i = numArray.Length;
+                            break;
+                        }
+
+                        if (numArray[i] > numArray[j]) {
+                            numArray[j] = '0';
+                            i = numArray.Length;
+                            break;
+                        }
+
+                    }
+                }
+
+                count++;
             }
-             answer = String.Join("", numList) ;
 
-            return answer;
+            StringBuilder answer = new StringBuilder();
+
+            foreach(var c in numArray) {
+                if (c != '0') answer.Append(c);
+            }
+
+            return answer.ToString();
         }
 
     }
