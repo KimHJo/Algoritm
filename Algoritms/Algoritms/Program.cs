956ab2f5f373cd51026465d50f78a5a9bb454528
KimHJo <dreamybaby@naver.com>
2019-08-08 13:39:58 +0900

    프로그래머스 C# 연습문제 Level1 하샤드수 콜라츠추측 : 특정값이상

--- Algoritms/Algoritms/Program.cs
+++ Algoritms/Algoritms/Program.cs
@@ -7,23 +7,45 @@ using System.Collections.Generic;
 namespace Algoritms {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine(solution(10));
+            Console.WriteLine(solution("1924", 2));
+            Console.WriteLine(solution("4177252841", 4));
         }
-        public static bool solution(int x) {
-            bool answer = true;
+        public static string solution(string number, int k) {
+            char[] temp = number.ToArray();
 
-            var ar = x.ToString().ToArray();
+            int numLen = number.ToString().Length;
 
-            int sum = 0;
-            foreach(var i in ar) {
-                sum += i;
+            for (int i = 0; i < k; i++) {
+                // i = 0 1 
+                for (int j = i; j < numLen - 2; j++) {
+                    // i = 0 : j = 0 1 2
+                    // i = 1 : j = 1 2
+                    for (int s = j + 1; s < numLen - 1; s++) {
+                        // i = 0 : j = 0 : s = 1 2 3
+                        // i = 0 : j = 1 : s = 2 3
+                        // i = 0 : j = 2 : s = 3
+
+                        // i = 1 : j = 1 : s = 2 3
+                        // i = 1 : j = 2 : s = 3
+                        if (temp[j] == temp[s]) {
+                            j++;
+                        } else if (temp[j] < temp[s]) {
+                            temp[j] = ' ';
+                            break;
+                        } else {
+                            temp[s] = ' ';
+                            break;
+                        }
+                    }
+                }
             }
 
-            
-            if (x % sum == 0) {
-                answer = true;
-            } else {
-                answer = false;
+            string answer = "";
+
+            foreach(char c in temp) {
+                if( c != ' ') {
+                    answer += c;
+                }
             }
 
             return answer;
