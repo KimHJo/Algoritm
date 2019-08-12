8371e923c3035414a1dbf8c5969239df69ddcb8c
KimHJo <dreamybaby@naver.com>
2019-08-12 10:21:07 +0900

    폴더 정리 / 스택으로 다시 시작

--- Algoritms/Algoritms/Program.cs
+++ Algoritms/Algoritms/Program.cs
@@ -10,53 +10,42 @@ namespace Algoritms {
         static void Main(string[] args) {
             //Console.WriteLine(solution("1924", 2));
             Console.WriteLine(solution("4177252841", 4));
+
         }
         public static string solution(string number, int k) {
+            // 0 인경우 예외처리
             if (number.All(x => x == '0'))
                 return "0";
 
-            char[] numArray = number.ToArray();
+            // k가 최대값인경우 예외처리
+            if (number.Length - k == 1)
+                return number.Max().ToString();
 
-            int count = 0;
-            while (count < k) {
-
-                for (int i = 0; i < numArray.Length-1; i++) {
-                    if (numArray[i] == '0')
-                        continue;
-
-                    for (int j = i+1; j < numArray.Length; j++) {
-                        if (numArray[j] == '0')
-                            continue;
-
-                        //Console.WriteLine("{0} {1} {2} {3}", i, j, numArray[i], numArray[j]);
-
-                        if(numArray[i] == numArray[j]) {
-
-                        }
-
-                        if(numArray[i] < numArray[j]) {
-                            numArray[i] = '0';
-                            i = numArray.Length;
-                            break;
-                        }
-
-                        if (numArray[i] > numArray[j]) {
-                            numArray[j] = '0';
-                            i = numArray.Length;
-                            break;
-                        }
-
-                    }
-                }
-
-                count++;
-            }
-
+            // 변수선언
+            Stack<char> numStack = new Stack<char>();
             StringBuilder answer = new StringBuilder();
+            List<char> numList = number.ToList<char>();
 
-            foreach(var c in numArray) {
-                if (c != '0') answer.Append(c);
+            // 예외처리로 인해 List[0] 는 항상 push 가능
+            numStack.Push(numList[0]);
+
+
+            for (int i = 0; i < k; i++) {
+
+
+                // 만약 peek 랑 배열값이 같으면 push
+                    // 만약 peek 보다 배열값이 작으면 push
+                    // peek 보다 배열값이 크면 pop 및 배열값 push
+                    // k번까지
+
+
+
+
             }
+
+            //foreach(var c in numArray) {
+            //    if (c != '0') answer.Append(c);
+            //}
 
             return answer.ToString();
         }
