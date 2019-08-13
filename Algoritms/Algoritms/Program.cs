69b5acc6b13effb74f5872ddc05dc447e909ac14
KimHJo <dreamybaby@naver.com>
2019-08-13 16:35:18 +0900

    프로그래머스 C# 연습문제 Level2 카펫 - 시작

--- Algoritms/Algoritms/Program.cs
+++ Algoritms/Algoritms/Program.cs
@@ -8,22 +8,10 @@ using System.Text;
 namespace Algoritms {
     class Program {
         static void Main(string[] args) {
-            solution("-4 -3 -2 1 -1 2 4 3");
         }
-        public static string solution(string s) {
-
-            string[] cArr = s.Split(' ').ToArray();
-
-            Array.Sort(cArr, (x, y) => {
-                int ix = int.Parse(x.ToString());
-                int iy = int.Parse(y.ToString());
-
-                return ix.CompareTo(iy);
-            }); 
-            
-            string answer = cArr[0] + " " + cArr[cArr.Length-1];
+        public static int[] solution(int brown, int red) {
+            int[] answer = new int[] { };
             return answer;
         }
-
     }
 }
