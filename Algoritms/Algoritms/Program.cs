e5b374fdf04d5d9d6de74bb7db0603160589faff
KimHJo <dreamybaby@naver.com>
2019-08-13 15:03:05 +0900

    프로그래머스 C# 연습문제 Level2 프린터 - 종료

--- Algoritms/Algoritms/Program.cs
+++ Algoritms/Algoritms/Program.cs
@@ -8,13 +8,6 @@ using System.Text;
 namespace Algoritms {
     class Program {
         static void Main(string[] args) {
-            //Console.WriteLine(solution(2, 10, new int[] { 7, 4, 5, 6 }));
-            Console.WriteLine(solution(new int[] { 2, 1, 3, 2 }, 2));
         }
-        public static int solution(int[] priorities, int location) {
-            int answer = 0;
-            return answer;
-        }
-
     }
 }
